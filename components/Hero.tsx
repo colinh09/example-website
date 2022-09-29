d969086ec358380325ef6cee61b643540539151e
colinh09 <colin.hwang@cooper.edu>
2022-09-29 00:48:45 -0400

    finished hero, looks nice

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Cursor, useTypewriter } from "react-simple-typewriter"
 import BackgroundCircles from './BackgroundCircles'
+import Image from 'next/image'
+import Link from 'next/link'
 
 type Props = {}
 
@@ -18,10 +20,28 @@ function Hero({}: Props) {
   return (
     <div className = "h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
         <BackgroundCircles />
-        <h1>
-            <span>{text}</span>
-            <Cursor cursorColor="#228C22" />
-        </h1>
+        <Image className="relative rounded-full mx-auto object-cover" alt="Picture of Colin Hwang" src= "/../public/me.jfif" width={170} height={170}/>
+        <div className="z-20">
+            <h2 className = "text-sm uppercase text-gray-500 pb-2 tracking-[15px]">Software Engineer</h2>
+            <h1 className = "text-4xl lg:text-5xl font-semibold px-10">
+                <span className="mr-3">{text}</span>
+                <Cursor cursorColor="#228C22" />
+            </h1>
+            <div className="pt-5">
+                <Link href="#about">
+                <button className="heroButton"> About </button>
+                </Link>
+                <Link href="#experience">
+                <button className="heroButton"> Experience </button>              
+                </Link>
+                <Link href="#skills">
+                <button className="heroButton"> Skills </button>                 
+                </Link>
+                <Link href="#projects">
+                <button className="heroButton"> Projects </button>           
+                </Link>
+            </div>
+        </div>
     </div>
   )
 }
